64a36a4992f83c6a960566079a46b34a8e88c609
ryk122 <54937624+ryk122@users.noreply.github.com>
2020-11-16 20:59:28 +0900

    add plist

---
+++ Assets/Editor/PostXcodeBuild.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace Hoge
+{
+    public class PostXcodeBuild
+    {
+        [PostProcessBuild]
+        public static void SetXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            if (buildTarget != BuildTarget.iOS) return;
+
+            var plistPath = pathToBuiltProject + "/Info.plist";
+            var plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+
+            var rootDict = plist.root;
+            // ここに記載したKey-ValueがXcodeのinfo.plistに反映されます
+            rootDict.SetString("GADApplicationIdentifier", "hogehoge");
+
+            File.WriteAllText(plistPath, plist.WriteToString());
+        }
+    }
+}
