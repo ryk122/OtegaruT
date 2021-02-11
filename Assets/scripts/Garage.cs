e333fd31a96693b2c6c94872224e7eb2e383bb5c
ryk122 <54937624+ryk122@users.noreply.github.com>
2021-02-11 18:40:26 +0900

    new car
    s80

--- Assets/scripts/Garage.cs
+++ Assets/scripts/Garage.cs
@@ -323,7 +323,7 @@ public class Garage : MonoBehaviour {
             t = 0; m = 0;
         }
 
-        if (dcar == 0 || dcar == 2  || dcar == 4 || dcar == 5 || dcar == 8 || dcar == 12 || dcar == 15)
+        if (dcar == 0 || dcar == 2  || dcar == 4 || dcar == 5 || dcar == 8 || dcar==11 || dcar == 12 || dcar == 15)
         {
             gt.ts.SetTune(t);
             tunebt.SetActive(true);
