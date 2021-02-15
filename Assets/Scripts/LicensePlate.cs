0ac20364dace26be0b5e22bceac97ecc248ce6a9
ryk122 <54937624+ryk122@users.noreply.github.com>
2021-02-15 13:36:26 +0900

    licensePlate

---
+++ Assets/Scripts/LicensePlate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LicensePlate : MonoBehaviour
+{
+    [SerializeField]
+    Text num, hira, subNum;
+    string[] data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int dcar = PlayerPrefs.GetInt("dcar");
+        string s = PlayerPrefs.GetString("license" + dcar);
+        data = s.Split(',');
+
+        if (data.Length >= 3)
+        {
+            num.text = data[0];
+            hira.text = data[1];
+            subNum.text = data[2];
+        }
+    }
+}
